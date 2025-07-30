f3bdac7e4aa104ae2e94f364ae6690ffade06137
mibe <mikhail.beck@exasol.com>
2025-07-30 11:37:11 +0100

    #6: Updated the parameter parser

--- exasol/ai/mcp/server/parameter_parser.py
+++ exasol/ai/mcp/server/parameter_parser.py
@@ -14,7 +14,9 @@ from pyexasol import ExaConnection
 from exasol.ai.mcp.server.parameter_pattern import (
     exa_type_pattern,
     identifier_pattern,
+    parameter_pattern,
     quoted_identifier_pattern,
+    quoted_parameter_pattern,
     regex_flags,
 )
 from exasol.ai.mcp.server.server_settings import MetaParameterSettings
@@ -22,6 +24,14 @@ from exasol.ai.mcp.server.utils import (
     report_error,
     sql_text_value,
 )
+
+parameter_list_pattern = rf"(?:,?\s*{quoted_parameter_pattern}\s*(?=\)|,))*"
+r"""
+A parameter list matching pattern, matching zero, one or more parameters. In the text,
+the list should be enclosed in parentheses. A parameter may follow a comma, if it's not
+the first one: The lookahead symbol after the parameter should be either a comma or a
+closing parenthesis: (?=\)|,).
+"""
 
 
 class ParameterParser(ABC):
@@ -31,6 +41,7 @@ class ParameterParser(ABC):
         self.connection = connection
         self.conf = conf
         self.tool_name = tool_name
+        self._parameter_list_pattern: re.Pattern | None = None
 
     def _execute_query(self, query: TextContent) -> list[dict[str, Any]]:
         return self.connection.meta.execute_snapshot(query=query).fetchall()
@@ -66,53 +77,52 @@ class ParameterParser(ABC):
         except Exception:  # pylint: disable=broad-exception-caught
             return report_error(self.tool_name, traceback.format_exc())
 
-    def parse_parameter_list(
-        self, params: str, allow_dynamic: bool = True, allow_double_quotes: bool = True
-    ) -> str | list[dict[str, str]]:
+    @property
+    def parameter_extract_pattern(self) -> re.Pattern:
+        r"""
+        Lazily compiles a pattern for extracting parameter names and their SQL types
+        from a parameter list.
+
+        The pattern is looking for a sequence of parameters, each starting either
+        from the beginning or from comma: (?:^|,). The parameter should be followed
+        by either the end of the input or comma: (?=\Z|,).
+        """
+        if self._parameter_list_pattern is not None:
+            return self._parameter_list_pattern
+
+        pattern = (
+            rf"(?:^|,)\s*(?P<{self.conf.name_field}>{quoted_identifier_pattern})"
+            rf"\s+(?P<{self.conf.type_field}>{exa_type_pattern})\s*(?=\Z|,)"
+        )
+        self._parameter_list_pattern = re.compile(pattern, flags=regex_flags)
+        return self._parameter_list_pattern
+
+    def parse_parameter_list(self, params: str) -> str | list[dict[str, str]]:
         """
         Breaks the input string into parameter definitions. The input string should be
         extracted from a text of a function or a script and contain a list of parameters,
-        where each parameter consists of a name and a type. The list can be either an
-        input or, in case of an EMIT UDF, the emit list.
+        where each parameter consists of a name and an SQL type. The list can be either
+        an input or, in case of an EMIT UDF, the emit list.
 
-        Normally, the function returns a list of dictionaries, where each dictionary
+        The parameters may be dynamic (variadic). This is designated with "...". In
+        such case, the function simply returns "...".
+
+        Otherwise, the function returns a list of dictionaries, where each dictionary
         describes a parameter and includes its name and type. The dictionary keys are
         defined by the provided configuration. The double quotes in the parameter name
         gets removed.
-
-        Args:
-            params:
-                Comma-separated list of parameters.
-            allow_dynamic:
-                If True, the parameters may be dynamic (variadic). This is designated
-                with "...". In such case, the function simply returns "...".
-            allow_double_quotes:
-                If True, the parameter name may be enclosed in double quotes.
         """
 
         def remove_double_quotes(di: dict[str, str]) -> dict[str, str]:
-            if allow_double_quotes:
-                return {key: val.strip('"') for key, val in di.items()}
-            return di
+            return {key: val.strip('"') for key, val in di.items()}
 
         params = params.lstrip()
-        if allow_dynamic and params == "...":
+        if params == "...":
             return params
 
-        # Parameter matching pattern is looking for a sequence that starts either
-        # from the beginning or from comma: (?:^|,), and consists of the parameter
-        # name and SQL type. That should follow either by the end of the string or
-        # comma: \s*(?=\Z|,).
-        param_name_pattern = (
-            quoted_identifier_pattern if allow_double_quotes else identifier_pattern
-        )
-        pattern = (
-            rf"(?:^|,)\s*(?P<{self.conf.name_field}>{param_name_pattern})"
-            rf"\s+(?P<{self.conf.type_field}>{exa_type_pattern})\s*(?=\Z|,)"
-        )
         return [
             remove_double_quotes(m.groupdict())
-            for m in re.finditer(pattern, params, flags=regex_flags)
+            for m in self.parameter_extract_pattern.finditer(params)
         ]
 
     def format_return_type(self, param: str) -> str | dict[str, str]:
@@ -143,6 +153,12 @@ class ParameterParser(ABC):
 
 class FuncParameterParser(ParameterParser):
 
+    def __init__(
+        self, connection: ExaConnection, conf: MetaParameterSettings, tool_name: str
+    ) -> None:
+        super().__init__(connection, conf, tool_name)
+        self._func_pattern: re.Pattern | None = None
+
     def get_func_query(self, schema_name: str, func_name: str) -> str:
         return dedent(
             f"""
@@ -152,29 +168,32 @@ class FuncParameterParser(ParameterParser):
         """
         )
 
-    def extract_parameters(self, info: dict[str, Any]) -> dict[str, Any] | None:
-        # The pattern matches zero, one or more instances of parameter pairs
-        # (name, type). A pair may start from a comma (for a parameter other than
-        # the first one): ,?. The lookahead symbol after the pair should be either
-        # a comma or a closing bracket: (?=\)|,).
-        parameter_list_pattern = (
-            rf"(?:,?\s*{identifier_pattern}\s+" rf"{exa_type_pattern}\s*(?=\)|,))*"
-        )
+    @property
+    def func_pattern(self) -> re.Pattern:
+        """
+        Lazily compiles the function parsing pattern.
+        """
+        if self._func_pattern is not None:
+            return self._func_pattern
+
+        func_schema_pattern = rf"(?:{quoted_identifier_pattern}\s*\.\s*)?"
+        func_name_pattern = quoted_identifier_pattern
         pattern = (
             r"\A\s*FUNCTION\s+"
-            rf'(?:(?:{info["FUNCTION_SCHEMA"]}|"{info["FUNCTION_SCHEMA"]}")\s*\.\s*)?'
-            rf'(?:{info["FUNCTION_NAME"]}|"{info["FUNCTION_NAME"]}")\s*'
+            rf"{func_schema_pattern}{func_name_pattern}\s*"
             rf"\((?P<{self.conf.input_field}>{parameter_list_pattern})\)\s*"
             rf"RETURN\s+(?P<{self.conf.return_field}>{exa_type_pattern})\s+"
         )
-        m = re.match(pattern, info["FUNCTION_TEXT"], flags=regex_flags)
+        self._func_pattern = re.compile(pattern, flags=regex_flags)
+        return self._func_pattern
+
+    def extract_parameters(self, info: dict[str, Any]) -> dict[str, Any] | None:
+        m = self.func_pattern.match(info["FUNCTION_TEXT"])
         if m is None:
             return None
         return {
             self.conf.input_field: self.parse_parameter_list(
-                m.group(self.conf.input_field),
-                allow_dynamic=False,
-                allow_double_quotes=False,
+                m.group(self.conf.input_field)
             ),
             self.conf.return_field: self.format_return_type(
                 m.group(self.conf.return_field)
@@ -183,6 +202,13 @@ class FuncParameterParser(ParameterParser):
 
 
 class ScriptParameterParser(ParameterParser):
+
+    def __init__(
+        self, connection: ExaConnection, conf: MetaParameterSettings, tool_name: str
+    ) -> None:
+        super().__init__(connection, conf, tool_name)
+        self._emit_pattern: re.Pattern | None = None
+        self._return_pattern: re.Pattern | None = None
 
     def get_func_query(self, schema_name: str, func_name: str) -> str:
         return dedent(
@@ -193,30 +219,55 @@ class ScriptParameterParser(ParameterParser):
         """
         )
 
-    def extract_parameters(self, info: dict[str, Any]) -> dict[str, Any] | None:
-        # The pattern matches the parameter list, as in FuncParameterParser, but with
-        # quoted parameter names.
-        parameter_list_pattern = (
-            rf"(?:,?\s*{quoted_identifier_pattern}\s+"
-            rf"{exa_type_pattern}\s*(?=\)|,))*"
-        )
-        # The pattern should also include the possibility of the variadic syntax: ...
+    def _udf_pattern(self, emits: bool) -> re.Pattern:
+        """Compiles a pattern for parsing a UDF script."""
+
+        # The parameter matching pattern should account for the possibility of the
+        # variadic syntax: ...
         dynamic_list_pattern = rf"(?:\s*...\s*|{parameter_list_pattern})"
-        if info["SCRIPT_RESULT_TYPE"] == "EMITS":
-            output_pattern = (
-                rf"\s*\((?P<{self.conf.emit_field}>{dynamic_list_pattern})\)\s*"
-            )
-        else:
-            output_pattern = rf"\s+(?P<{self.conf.return_field}>{exa_type_pattern})\s+"
-        pattern = (
-            rf'\A\s*CREATE\s+{info["SCRIPT_LANGUAGE"]}\s+'
-            rf'{info["SCRIPT_INPUT_TYPE"]}\s+SCRIPT\s+'
-            rf'(?:(?:{info["SCRIPT_SCHEMA"]}|"{info["SCRIPT_SCHEMA"]}")\s*\.\s*)?'
-            rf'(?:{info["SCRIPT_NAME"]}|"{info["SCRIPT_NAME"]}")\s*'
-            rf"\((?P<{self.conf.input_field}>{dynamic_list_pattern})\)\s*"
-            rf'{info["SCRIPT_RESULT_TYPE"]}{output_pattern}AS\s+'
+
+        output_pattern = (
+            rf"EMITS\s*\((?P<{self.conf.emit_field}>{dynamic_list_pattern})\)\s*"
+            if emits
+            else rf"RETURNS\s+(?P<{self.conf.return_field}>{exa_type_pattern})\s+"
         )
-        m = re.match(pattern, info["SCRIPT_TEXT"], flags=regex_flags)
+        language_pattern = identifier_pattern
+        udf_schema_pattern = rf"(?:{quoted_identifier_pattern}\s*\.\s*)?"
+        udf_name_pattern = quoted_identifier_pattern
+
+        pattern = (
+            rf"\A\s*CREATE\s+{language_pattern}\s+(?:SCALAR|SET)\s+SCRIPT\s+"
+            rf"{udf_schema_pattern}{udf_name_pattern}\s*"
+            rf"\((?P<{self.conf.input_field}>{dynamic_list_pattern})\)\s*"
+            rf"{output_pattern}AS\s+"
+        )
+        return re.compile(pattern, flags=regex_flags)
+
+    @property
+    def emit_udf_pattern(self) -> re.Pattern:
+        """
+        Lazily compiles the Emit-UDF parsing pattern.
+        """
+        if self._emit_pattern is None:
+            self._emit_pattern = self._udf_pattern(emits=True)
+        return self._emit_pattern
+
+    @property
+    def return_udf_pattern(self) -> re.Pattern:
+        """
+        Lazily compiles the Return-UDF parsing pattern.
+        """
+        if self._return_pattern is None:
+            self._return_pattern = self._udf_pattern(emits=False)
+        return self._return_pattern
+
+    def extract_parameters(self, info: dict[str, Any]) -> dict[str, Any] | None:
+        pattern = (
+            self.emit_udf_pattern
+            if info["SCRIPT_RESULT_TYPE"] == "EMITS"
+            else self.return_udf_pattern
+        )
+        m = pattern.match(info["SCRIPT_TEXT"])
         if m is None:
             return None
         param_func = {
