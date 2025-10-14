5805ee0cf75a2b5fb7864c641ba3fe954020bbb0
mibe <mikhail.beck@exasol.com>
2025-10-14 17:19:51 +0100

    #67: addressed review comments

--- noxfile.py
+++ noxfile.py
@@ -1,5 +1,3 @@
-import sys
-
 import nox
 
 # imports all nox task provided by the toolbox
@@ -7,16 +5,3 @@ from exasol.toolbox.nox.tasks import *
 
 # default actions to be run if nothing is explicitly specified with the -s option
 nox.options.sessions = ["project:fix"]
-
-
-@nox.session(name="test:integration_mcp", python=False)
-def integration_mcp(session: nox.Session):
-    if "." not in sys.path:
-        sys.path.append(".")
-    from test.utils.mcp_oidc_constants import DOCKER_JWK_URL
-
-    extended_args = list(session.posargs) + [
-        "---itde-additional-db-parameter",
-        DOCKER_JWK_URL,
-    ]
-    session.notify("test:integration", extended_args)
