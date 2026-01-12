9183eabc48888d51ecab9681926e51035f0a7e00
mibe <mikhail.beck@exasol.com>
2026-01-12 14:35:08 +0000

    #120: Added test for the download_file tool

--- test/integration/test_mcp_bucketfs.py
+++ test/integration/test_mcp_bucketfs.py
@@ -1,4 +1,5 @@
 import asyncio
+import time
 from collections.abc import Generator
 from contextlib import contextmanager
 from dataclasses import dataclass
@@ -9,6 +10,7 @@ from test.utils.db_objects import (
 )
 from test.utils.result_utils import (
     get_list_result_json,
+    get_result_content,
     result_sort_func,
 )
 from unittest.mock import (
@@ -20,6 +22,7 @@ import exasol.bucketfs as bfs
 import pyexasol
 import pytest
 from fastmcp import Client
+from fastmcp.client.elicitation import ElicitResult
 from fastmcp.exceptions import ToolError
 
 from exasol.ai.mcp.server.bucketfs_tools import (
@@ -53,11 +56,19 @@ _chimpanzee = (
     "large ears."
 )
 
+_home_luminis = (
+    "These robust hominins possessed elongated, chisel-like fingernails of dense "
+    "keratin, which they used to strip bark and dig for subterranean tubers. Their "
+    "most distinctive feature was a network of bioluminescent patches along the "
+    "shoulders and neck, likely used for social signaling in the dim forests they "
+    "called home."
+)
+
 
 @dataclass
 class ElicitationData:
     warning: bool
-    action: str
+    action: str | None
     data: dict[str, str]
 
 
@@ -67,9 +78,27 @@ class WriteTestCase:
     content: str
     elicitation: list[ElicitationData]
 
+    def _expected_value(self, init_value, value_name: str) -> str:
+        x_value = init_value
+        for ed in self.elicitation:
+            if value_name in ed.data:
+                x_value = ed.data[value_name]
+        return x_value
+
+    @property
+    def expected_path(self) -> str:
+        return self._expected_value(self.path, "file_path")
+
+    @property
+    def expected_content(self) -> str:
+        return self._expected_value(self.content, "file_content")
+
 
 async def _run_tool_async(
-    bucketfs_location: bfs.path.PathLike, tool_name: str, **kwargs
+    bucketfs_location: bfs.path.PathLike,
+    tool_name: str,
+    elicitation: list[ElicitationData] | None = None,
+    **kwargs,
 ):
     elicit_count = 0
 
@@ -79,10 +108,10 @@ async def _run_tool_async(
 
     async def elicitation_handler(message: str, response_type: type, params, context):
         nonlocal elicit_count
-        elicitation: ElicitationData = kwargs["elicitation"][elicit_count]
-        assert (OVERWRITE_WARNING in message) == elicitation.warning
-        action = elicitation.action
-        response_data = response_type(**elicitation.data)
+        elicitation_i = elicitation[elicit_count]
+        assert (OVERWRITE_WARNING in message) == elicitation_i.warning
+        action = elicitation_i.action
+        response_data = response_type(**elicitation_i.data)
         elicit_count += 1
         return ElicitResult(action=action, content=response_data)
 
@@ -90,13 +119,20 @@ async def _run_tool_async(
 
     config = McpServerSettings(enable_read_bucketfs=True, enable_write_bucketfs=True)
     exa_server = create_mcp_server(db_connection, config, bucketfs_location)
-    elicit_handler = elicitation_handler if elicitation in kwargs else None
+    elicit_handler = elicitation_handler if elicitation is not None else None
     async with Client(exa_server, elicitation_handler=elicit_handler) as client:
         return await client.call_tool(tool_name, kwargs)
 
 
-def _run_tool(bucketfs_location: bfs.path.PathLike, tool_name: str, **kwargs):
-    return asyncio.run(_run_tool_async(bucketfs_location, tool_name, **kwargs))
+def _run_tool(
+    bucketfs_location: bfs.path.PathLike,
+    tool_name: str,
+    elicitation: list[ElicitationData] | None = None,
+    **kwargs,
+):
+    return asyncio.run(
+        _run_tool_async(bucketfs_location, tool_name, elicitation, **kwargs)
+    )
 
 
 def _get_expected_list_json(items: dict[str, ExaBfsObject]) -> ExaDbResult:
@@ -229,10 +265,32 @@ def test_find_files(bucketfs_location, bfs_data, path) -> None:
     assert result_json == expected_json
 
 
+def test_read_file(bucketfs_location, bfs_data) -> None:
+    for item in bfs_data.items:
+        if isinstance(item, ExaBfsDir):
+            dir_path = f"{bfs_data.name}/{item.name}"
+            for sub_item in item.items:
+                if isinstance(sub_item, ExaBfsFile):
+                    file_path = f"{dir_path}/{sub_item.name}"
+                    result = _run_tool(bucketfs_location, "read_file", path=file_path)
+                    content = get_result_content(result)
+                    assert content == str(sub_item.content, encoding="utf-8")
+
+
 @pytest.mark.parametrize(
     "test_case",
     [
         WriteTestCase(
+            # A simple case with one round of elicitation.
+            path="Species/Primates/chimpanzee",
+            content=_chimpanzee,
+            elicitation=[ElicitationData(warning=False, action="accept", data={})],
+        ),
+        WriteTestCase(
+            # First suggests a path that doesn't exist,
+            # then, in the elicitation, changes it to another one that does exist.
+            # This should cause another elicitation, where it changes it again.
+            # The content is changed in the first elicitation.
             path="Species/Primates/human",
             content=_chimpanzee,
             elicitation=[
@@ -241,23 +299,109 @@ def test_find_files(bucketfs_location, bfs_data, path) -> None:
                     action="accept",
                     data={
                         "file_path": "Species/Rodents/Squirrel",
-                        "file_content": _chimpanzee,
+                        "file_content": _human,
                     },
                 ),
                 ElicitationData(
                     warning=True,
                     action="accept",
-                    data={"file_path": path, "file_content": _human},
+                    data={"file_path": "Species/Primates/human"},
                 ),
             ],
-        )
+        ),
     ],
+    ids=["one elicitation", "two elicitations"],
 )
 def test_write_file(bucketfs_location, test_case) -> None:
-    result = _run_tool(
+    _run_tool(
         bucketfs_location,
         "write_file",
+        elicitation=test_case.elicitation,
         path=test_case.path,
         content=test_case.content,
-        elicitation=test_case.elicitation,
     )
+    try:
+        result = _run_tool(bucketfs_location, "read_file", path=test_case.expected_path)
+        content = get_result_content(result)
+        assert content == test_case.expected_content
+    finally:
+        expected_bfs_path = bucketfs_location.joinpath(test_case.expected_path)
+        expected_bfs_path.rm()
+
+
+@pytest.mark.parametrize("action", ["decline", "cancel", None])
+def test_write_file_not_accepted(bucketfs_location, action) -> None:
+    path = "Species/Primates/home_luminis"
+    elicitation = [
+        ElicitationData(warning=False, action=action, data={}),
+    ]
+    with pytest.raises(ToolError):
+        _run_tool(
+            bucketfs_location,
+            "write_file",
+            elicitation=elicitation,
+            path=path,
+            content=_home_luminis,
+        )
+    bfs_path = bucketfs_location.joinpath(path)
+    assert not bfs_path.exists()
+
+
+@pytest.mark.parametrize(
+    "test_case",
+    [
+        WriteTestCase(
+            path="download_test/README",
+            content="",
+            elicitation=[ElicitationData(warning=False, action="accept", data={})],
+        ),
+        WriteTestCase(
+            path="Species/Rodents/Squirrel",
+            content="",
+            elicitation=[
+                ElicitationData(
+                    warning=True,
+                    action="accept",
+                    data={
+                        "file_path": "README",
+                    },
+                ),
+            ],
+        ),
+    ],
+    ids=["accept path", "change path"],
+)
+def test_download_file(bucketfs_location, test_case) -> None:
+    _run_tool(
+        bucketfs_location,
+        "download_file",
+        elicitation=test_case.elicitation,
+        url="https://raw.githubusercontent.com/octocat/Hello-World/master/README",
+        path=test_case.path,
+    )
+    time.sleep(5)
+    try:
+        result = _run_tool(bucketfs_location, "read_file", path=test_case.expected_path)
+        content = get_result_content(result)
+        assert content.strip() == "Hello World!"
+    finally:
+        expected_bfs_path = bucketfs_location.joinpath(test_case.expected_path)
+        expected_bfs_path.rm()
+
+
+@pytest.mark.parametrize("action", ["decline", "cancel", None])
+def test_download_file_not_accepted(bucketfs_location, action) -> None:
+    path = "download_test_rejected/README"
+    elicitation = [
+        ElicitationData(warning=False, action=action, data={}),
+    ]
+    with pytest.raises(ToolError):
+        _run_tool(
+            bucketfs_location,
+            "download_file",
+            elicitation=elicitation,
+            url="https://raw.githubusercontent.com/octocat/Hello-World/master/README",
+            path=path,
+        )
+    bfs_path = bucketfs_location.joinpath(path)
+    assert not bfs_path.exists()
