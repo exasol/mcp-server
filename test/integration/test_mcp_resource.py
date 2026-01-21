0e710e0b302e18027a2be8dfea9ffb3dd2ab9951
mibe <mikhail.beck@exasol.com>
2026-01-21 08:33:19 +0000

    #135: Addressed other review comments

--- test/integration/test_mcp_resource.py
+++ test/integration/test_mcp_resource.py
@@ -34,5 +34,13 @@ def _get_resource_content(result) -> str:
 def test_list_sql_types(pyexasol_connection):
     result = _read_resource(pyexasol_connection, "dialect://sql-types")
     result_json = get_list_result_json(result, _get_resource_content)
-    type_names = [row["TYPE_NAME"] for row in result_json.result]
-    assert all(type_name in type_names for type_name in ["CHAR", "VARCHAR", "DECIMAL"])
+    test_types = ["CHAR", "VARCHAR", "DECIMAL"]
+    test_data = list(
+        filter(lambda row: row["TYPE_NAME"] in test_types, result_json.result)
+    )
+    # Verify that all test types are present in the output.
+    assert {row["TYPE_NAME"] for row in test_data} == set(test_types)
+    # Verify that there are values in all other expected columns.
+    for col_name in ["CREATE_PARAMS", "PRECISION"]:
+        for row in test_data:
+            assert row[col_name], f'{col_name} is empty for type {row["TYPE_NAME"]}'
