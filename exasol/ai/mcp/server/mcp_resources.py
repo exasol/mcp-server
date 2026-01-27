c6abf13fbbe2dca0ad2836f127974e6ac5215718
mibe <mikhail.beck@exasol.com>
2026-01-27 10:30:14 +0000

    #146: Fixe resource path

--- exasol/ai/mcp/server/mcp_resources.py
+++ exasol/ai/mcp/server/mcp_resources.py
@@ -4,7 +4,8 @@ from typing import Any
 
 from exasol.ai.mcp.server.server_settings import ExaDbResult
 
-BUILTIN_FUNCTIONS_JSON = "resources/exasol_builtin_functions.json"
+BUILTIN_FUNCTIONS_JSON = "exasol_builtin_functions.json"
+PACKAGE_RESOURCES = f"{__package__}.resources"
 
 
 def list_builtin_functions(category: str) -> ExaDbResult:
@@ -12,7 +13,7 @@ def list_builtin_functions(category: str) -> ExaDbResult:
     Selects the list of builtin functions of the specified type (category), reading
     the resource json. Only takes the name and the description fields.
     """
-    with importlib.resources.open_text(__package__, BUILTIN_FUNCTIONS_JSON) as f:
+    with importlib.resources.open_text(PACKAGE_RESOURCES, BUILTIN_FUNCTIONS_JSON) as f:
         func_list: list[dict[str, Any]] = json.load(f)
     allowed_fields = ["name", "description"]
     category = category.lower()
@@ -34,7 +35,7 @@ def describe_builtin_function(name: str) -> ExaDbResult:
     Returns all fields. Some functions, for example TO_CHAR, can have information in
     more than one row.
     """
-    with importlib.resources.open_text(__package__, BUILTIN_FUNCTIONS_JSON) as f:
+    with importlib.resources.open_text(PACKAGE_RESOURCES, BUILTIN_FUNCTIONS_JSON) as f:
         func_list: list[dict[str, Any]] = json.load(f)
     name = name.upper()
     selected_info = list(filter(lambda func_info: func_info["name"] == name, func_list))
