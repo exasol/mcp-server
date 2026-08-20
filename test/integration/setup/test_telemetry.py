6b44076414c57baf2f09b24ce7f7b31f33f7abff
mibe <mikhail.beck@exasol.com>
2026-08-20 08:48:35 +0100

    #253: Removed temporary no-telemetry test

--- test/integration/setup/test_telemetry.py
+++ test/integration/setup/test_telemetry.py
@@ -44,21 +44,3 @@ def test_setup_telemetry_sends_started_event(httpserver, monkeypatch) -> None:
     request, _ = httpserver.log[0]
     features = json.loads(request.get_data())["features"]
     assert f"{_PROJECT_SHORT_TAG}.started" in features
-
-
-# TODO(#253): temporary, remove after manually confirming the disabled path
-# behaves as expected.
-# @pytest.mark.parametrize("envar", [ENV_CI, ENV_DISABLE])
-def test_setup_telemetry_disabled_sends_nothing(httpserver, monkeypatch) -> None:
-    """
-    When telemetry is disabled, ``setup_telemetry`` must not deliver the
-    "started" event.
-    """
-    monkeypatch.setenv(ENV_ENDPOINT, httpserver.url_for("/telemetry"))
-    # monkeypatch.setenv(envar, "true")
-    httpserver.expect_request("/telemetry", method="POST").respond_with_json({})
-
-    setup_telemetry(logging.getLogger("test_setup_telemetry_disabled"))
-    telemetry.shutdown(flush_buffers=True)
-
-    assert len(httpserver.log) == 0
